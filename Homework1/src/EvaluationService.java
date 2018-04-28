f94a0d0c32a0ad60b80d78ba65a76a6457afd946
zmacaulay <zmacaulay@gmail.com>
2018-04-28 16:59:13 -0400

    Scrabble score complete

--- Homework1/src/EvaluationService.java
+++ Homework1/src/EvaluationService.java
@@ -125,7 +125,54 @@ public class EvaluationService {
 	 */
 	public int getScrabbleScore(String string) {
 		// TODO Write an implementation for this method declaration
-		return 0;
+		int score = 0;
+		
+		for(int i=0; i < string.length(); i++) {
+			char letter = string.charAt(i);
+			switch(letter) {
+				case 'a':
+				case 'e':
+				case 'i':
+				case 'o':
+				case 'u':
+				case 'l':
+				case 'n':
+				case 'r':
+				case 's':
+				case 't':
+					score += 1;
+					break;
+				case 'd':
+				case 'g':
+					score += 2;
+					break;
+				case 'b':
+				case 'c':
+				case 'm':
+				case 'p':
+					score += 3;
+					break;
+				case 'f':
+				case 'h':
+				case 'v':
+				case 'w':
+				case 'y':
+					score += 4;
+					break;
+				case 'k':
+					score += 5;
+					break;
+				case 'j':
+				case 'x':
+					score += 8;
+					break;
+				case 'q':
+				case 'z':
+					score += 10;
+					break;
+			}
+		}
+		return score;
 	}
 
 	/**
--- Homework1/src/TestCode.java
+++ Homework1/src/TestCode.java
@@ -1,132 +1,82 @@
 
 public class TestCode {
-
-	public static void main(String[] args) {
-		// Number 20
-		String s = "What is 5 plus 13?";
-		String t = "What is 7 minus 5?";
-		String u = "What is 6 multiplied by 4?";
-		String v = "What is 25 divided by 5?";
-		int resultAdd = solveWordProblem(s);
-		int resultSub = solveWordProblem(t);
-		int resultMulti = solveWordProblem(u);
-		int resultDiv = solveWordProblem(v);
-		System.out.println(s);
-		System.out.println(resultAdd);
+	/**
+	 * 4. Given a word, compute the scrabble score for that word.
+	 * 
+	 * --Letter Values-- Letter Value A, E, I, O, U, L, N, R, S, T = 1; D, G = 2; B,
+	 * C, M, P = 3; F, H, V, W, Y = 4; K = 5; J, X = 8; Q, Z = 10; Examples
+	 * "cabbage" should be scored as worth 14 points:
+	 * 
+	 * 3 points for C, 1 point for A, twice 3 points for B, twice 2 points for G, 1
+	 * point for E And to total:
+	 * 
+	 * 3 + 2*1 + 2*3 + 2 + 1 = 3 + 2 + 6 + 3 = 5 + 9 = 14
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public static int getScrabbleScore(String string) {
+		// TODO Write an implementation for this method declaration
+		int score = 0;
 		
-		System.out.println(t);
-		System.out.println(resultSub);
-		
-		System.out.println(u);
-		System.out.println(resultMulti);
-		
-		System.out.println(v);
-		System.out.println(resultDiv);
+		for(int i=0; i < string.length(); i++) {
+			char letter = string.charAt(i);
+			switch(letter) {
+				case 'a':
+				case 'e':
+				case 'i':
+				case 'o':
+				case 'u':
+				case 'l':
+				case 'n':
+				case 'r':
+				case 's':
+				case 't':
+					score += 1;
+					break;
+				case 'd':
+				case 'g':
+					score += 2;
+					break;
+				case 'b':
+				case 'c':
+				case 'm':
+				case 'p':
+					score += 3;
+					break;
+				case 'f':
+				case 'h':
+				case 'v':
+				case 'w':
+				case 'y':
+					score += 4;
+					break;
+				case 'k':
+					score += 5;
+					break;
+				case 'j':
+				case 'x':
+					score += 8;
+					break;
+				case 'q':
+				case 'z':
+					score += 10;
+					break;
+			}
+		}
+		return score;
 	}
 	
-	public static int solveWordProblem(String string) {
+	public static void main(String[] args) {
+		int scoreTest1 = getScrabbleScore("f");
+		System.out.println(scoreTest1);
 		
-		String[] twoNums = string.split(" ");
+		int scoreTest2 = getScrabbleScore("zoo");
+		System.out.println(scoreTest2);
 		
-		int num1 = Integer.parseInt(twoNums[2]);
-		String numberString = "";
-		if (twoNums[3].equals("plus")) {
-			for (int i=0; i<twoNums[4].length(); i++) {
-				switch(twoNums[4].charAt(i)) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-				case '-':
-					numberString += twoNums[4].charAt(i);
-					break;
-				default:
-				}
-			}
-			int num2 = Integer.parseInt(numberString);
-			int sum = num1+num2;
-			return sum;
-		}
+		int scoreTest3 = getScrabbleScore("street");
+		System.out.println(scoreTest3);
 		
-		else if (twoNums[3].equals("minus")) {
-			for (int i=0; i<twoNums[4].length(); i++) {
-				switch(twoNums[4].charAt(i)) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-				case '-':
-					numberString += twoNums[4].charAt(i);
-					break;
-				default:
-				}
-			}
-			int num2 = Integer.parseInt(numberString);
-			int difference = num1-num2;
-			return difference;
-		}
-		
-		else if (twoNums[3].equals("multiplied")) {
-			for (int i=0; i<twoNums[5].length(); i++) {
-				switch(twoNums[5].charAt(i)) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-				case '-':
-					numberString += twoNums[5].charAt(i);
-					break;
-				default:
-				}
-			}
-			int num2 = Integer.parseInt(numberString);
-			int product = num1*num2;
-			return product;
-		}
-		
-		else if (twoNums[3].equals("divided")) {
-			for (int i=0; i<twoNums[5].length(); i++) {
-				switch(twoNums[5].charAt(i)) {
-				case '0':
-				case '1':
-				case '2':
-				case '3':
-				case '4':
-				case '5':
-				case '6':
-				case '7':
-				case '8':
-				case '9':
-				case '-':
-					numberString += twoNums[5].charAt(i);
-					break;
-				default:
-				}
-			}
-			int num2 = Integer.parseInt(numberString);
-			int quotient = num1/num2;
-			return quotient;
-		}
-		
-		return 0;
 	}
 
 }
